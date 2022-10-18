27b6720acba1a663a4a827b6d2db6c71b503477c
Yiyun Qin <yiyun.qin@mths.ca>
2022-10-18 13:42:37 +0000

    invalid type and cannot be used

---
+++ src/MrCoxallStack.ts
@@ -0,0 +1,54 @@
+/**
+ * This is the stack
+ *
+ * By:      Yiyun Qin
+ * Version: 1.0
+ * Since:   2022-09-23
+ */
+
+class MrCoxallStack {
+  stackAsList: number[] = []
+
+  // getter
+  getList(): number[] | string {
+    if (this.stackAsList === undefined) {
+      return 'nothing'
+    } else {
+      return this.stackAsList
+    }
+  }
+
+  // setter
+  pushItem(pushedNumber: number): void {
+    this.stackAsList.push(pushedNumber)
+  }
+
+  popItem(): any {
+    let popNumber: number | undefined
+    if (this.stackAsList === undefined) {
+      console.log('Nothing in the list!')
+    } else {
+      popNumber = this.stackAsList.pop()
+      return popNumber
+    }
+  }
+
+  peekItem(): any {
+    let peekNumber: number | undefined
+    if (this.stackAsList === undefined) {
+      console.log('Nothing in the list!')
+    } else {
+      peekNumber = this[this.stackAsList.length - 1]
+      return peekNumber
+    }
+  }
+
+  showItem(): void {
+    if (this.stackAsList === undefined) {
+      console.log('Nothing in the list!')
+    } else {
+      console.log(this.stackAsList)
+    }
+  }
+}
+export = MrCoxallStack
